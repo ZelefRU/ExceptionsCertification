42c541bf908a6b6c859e84c88a21bce6fb6de122
Zelef <crazzzy0meat@gmail.com>
2023-10-31 14:40:28 +0600

    Initial commit

---
+++ src/main/java/org/example/View.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class View {
+
+    public static void showRequest() {
+        System.out.println("""
+                - Введите данные пользователя -
+                Формат: Фамилия Имя Отчество ДатаРождения(dd.mm.yyyy) НомерТелефона(должен начинаться с +) 
+                """);
+    }
+
+}
